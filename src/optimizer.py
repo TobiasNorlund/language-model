ee8c381cd8d261708723da6a85cafd42c39a8a72
Tobias Norlund <tobias.norlund@schibsted.com>
2019-10-17 18:05:12 +0000

    exp=31

--- src/optimizer.py
+++ src/optimizer.py
@@ -11,6 +11,7 @@ hp.add("momentum", 0.0, help="Momentum for optimizer")
 hp.add("adam_beta_1", 0.9, help="Beta 1 for Adam optimizer")
 hp.add("adam_beta_2", 0.999, help="Beta 2 for Adam optimizer")
 hp.add("rmsprop_rho", 0.9, help="Rho value for RMSprop")
+hp.add("clipnorm", None, dtype=float, help="Clip the gradient to the specified l2 norm")
 
 
 def get_optimizer():
@@ -19,14 +20,17 @@ def get_optimizer():
         if hp.get("learning_rate_schedule") else hp.get("learning_rate")
     if hp.get("optimizer") == "sgd":
         return tf.keras.optimizers.SGD(learning_rate,
-                                       hp.get("momentum")), learning_rate
+                                       hp.get("momentum"), clipnorm=hp.get("clipnorm")), learning_rate
     elif hp.get("optimizer") == "adam":
         return tf.keras.optimizers.Adam(learning_rate,
                                         hp.get("adam_beta_1"),
-                                        hp.get("adam_beta_2")), learning_rate
+                                        hp.get("adam_beta_2"),
+                                        clipnorm=hp.get("clipnorm")), learning_rate
     elif hp.get("optimizer") == "rmsprop":
         return tf.keras.optimizers.RMSprop(learning_rate,
                                            hp.get("rmsprop_rho"),
-                                           hp.get("momentum")), learning_rate
+                                           hp.get("momentum"),
+                                           clipnorm=hp.get("clipnorm")), learning_rate
     elif hp.get("optimizer") == "adagrad":
-        return tf.keras.optimizers.Adagrad(learning_rate), learning_rate
+        return tf.keras.optimizers.Adagrad(learning_rate,
+                                           clipnorm=hp.get("clipnorm")), learning_rate
